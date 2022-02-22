579424baf59d007ad77549e9392a4f467029fae5
Briana-Koo <RoyalBlueSnow@gmail.com>
2022-02-22 10:52:09 -0600

    Added user location

--- Map Hackwich/LocationManager.swift
+++ Map Hackwich/LocationManager.swift
@@ -5,3 +5,15 @@
 //  Created by Student on 2/22/22.
 //
 
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    var locationManager = CLLocationManager()
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
